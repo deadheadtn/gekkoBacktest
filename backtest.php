ad955f983344968101bd848d5496cca0549f3f1f
nejmeddine khechine <nejmpotter@gmail.com>
2019-02-07 20:31:45 +0000

    arguments

--- backtest.php
+++ backtest.php
@@ -1,63 +1,5 @@
 <?php
 
-function launchebot($pairs)
-	{
-	$from = $pairs['to'];
-	$jsonwatcher = '{"watch":{"exchange":"binance","currency":"' . $pairs["currency"] . '","asset":"' . $pairs["asset"] . '"},"candleWriter":{"enabled":true,"adapter":"sqlite"},"type":"market watcher","mode":"realtime"}';
-	$jsonpaper = '{"market":{"type":"leech","from":"' . $from . '"},"mode":"realtime","watch":{"exchange":"binance","currency":"' . $pairs["currency"] . '","asset":"' . $pairs["asset"] . '"},"tradingAdvisor":{"enabled":true,"method":"mounirs-ga-version-2","candleSize":' . $pairs['candlesize'] . ',"historySize":10},"x2_rsi":{"interval":14,"thresholds":{"low":30,"high":70,"persistence":1}},"paperTrader":{"feeMaker":0.25,"feeTaker":0.25,"feeUsing":"maker","slippage":0.05,"simulationBalance":{"asset":1,"currency":100},"reportRoundtrips":true,"enabled":true},"candleWriter":{"enabled":true,"adapter":"sqlite"},"type":"paper trader","performanceAnalyzer":{"riskFreeReturn":2,"enabled":true},"valid":true}';
-	$connection = curl_init();
-	curl_setopt($connection, CURLOPT_URL, "http://localhost:3000/api/startGekko");
-	curl_setopt($connection, CURLOPT_POST, true);
-	curl_setopt($connection, CURLOPT_POSTFIELDS, $jsonwatcher);
-	curl_setopt($connection, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($connection, CURLOPT_ENCODING, 'gzip, deflate');
-	curl_setopt($connection, CURLOPT_TIMEOUT, -1);
-	$headers = array();
-	$headers[] = "Origin: http://localhost:3000/api/startGekko";
-	$headers[] = "Accept-Encoding: gzip, deflate, br";
-	$headers[] = "Accept-Language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7";
-	$headers[] = "X-Hola-Request-Id: 80131";
-	$headers[] = "X-Requested-With: XMLHttpRequest";
-	$headers[] = "Connection: keep-alive";
-	$headers[] = "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36";
-	$headers[] = "Content-Type: application/json";
-	$headers[] = "Accept: */*";
-	$headers[] = "Cache-Control: no-cache,no-store,must-revalidate,max-age=-1,private";
-	$headers[] = "Referer: http://localhost:3000/api/startGekko";
-	$headers[] = "Dnt: 1";
-	$headers[] = "X-Hola-Unblocker-Bext: reqid 80131: before request, vpn is not allowed, send headers";
-	$headers[] = "Expires: -1";
-	curl_setopt($connection, CURLOPT_HTTPHEADER, $headers);
-	$responsej = curl_exec($connection);
-
-	//
-
-	$connection = curl_init();
-	curl_setopt($connection, CURLOPT_URL, "http://localhost:3000/api/startGekko");
-	curl_setopt($connection, CURLOPT_POST, true);
-	curl_setopt($connection, CURLOPT_POSTFIELDS, $jsonpaper);
-	curl_setopt($connection, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($connection, CURLOPT_ENCODING, 'gzip, deflate');
-	curl_setopt($connection, CURLOPT_TIMEOUT, -1);
-	$headers = array();
-	$headers[] = "Origin: http://localhost:3000/api/startGekko";
-	$headers[] = "Accept-Encoding: gzip, deflate, br";
-	$headers[] = "Accept-Language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7";
-	$headers[] = "X-Hola-Request-Id: 80131";
-	$headers[] = "X-Requested-With: XMLHttpRequest";
-	$headers[] = "Connection: keep-alive";
-	$headers[] = "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36";
-	$headers[] = "Content-Type: application/json";
-	$headers[] = "Accept: */*";
-	$headers[] = "Cache-Control: no-cache,no-store,must-revalidate,max-age=-1,private";
-	$headers[] = "Referer: http://localhost:3000/api/startGekko";
-	$headers[] = "Dnt: 1";
-	$headers[] = "X-Hola-Unblocker-Bext: reqid 80131: before request, vpn is not allowed, send headers";
-	$headers[] = "Expires: -1";
-	curl_setopt($connection, CURLOPT_HTTPHEADER, $headers);
-	$responsej = curl_exec($connection);
-	}
-
 function multiRequest($data, $options = array())
 	{
 	$headers = array();
@@ -223,7 +165,7 @@ for ($i = 0; $i < count($data); $i++)
 	foreach($stratsegyarray as $strat)
 		{
 			$max=0;
-		if (isset($data[$i]['ranges'][0]) && strcmp($data[$i]["currency"], "BTC") == 0 && strcmp($data[$i]["asset"], "BNB") == 0 )
+		if (isset($data[$i]['ranges'][0]) && strcmp($data[$i]["currency"], $argv[1]) == 0 && strcmp($data[$i]["asset"], $argv[2]) == 0 )
 			{
 			$strategy = json_decode(json_encode(Toml::parseFile("Param/" . $strat)) , true);
 			$strategy_loaded = json_decode(json_encode(Toml::parseFile("params/" . $strat)) , true);
@@ -242,7 +184,7 @@ for ($i = 0; $i < count($data); $i++)
 					$tradeCount = 0;
 					$strategy_load = loadf($pp, $strategy_loaded);
 						$tt->setTimestamp($data[$i]['ranges'][0]['to']);
-						$from = date("Y-m-d\\TH:i:sP", strtotime('-125 days', $tt->getTimestamp()));
+						$from = date("Y-m-d\\TH:i:sP", strtotime('-'.$argv[2].' days', $tt->getTimestamp()));
 		        $to = $tt->format('Y-m-d H:i');
 						$j++;
 						$ttt = '{"watch":{"exchange":"binance","currency":"' . $data[$i]["currency"] . '","asset":"' . $data[$i]["asset"] . '"},"paperTrader":{"feeMaker":0.15,"feeTaker":0.15,"feeUsing":"maker","slippage":0.05,"simulationBalance":{"asset":1,"currency":100},"reportRoundtrips":true,"enabled":true},"tradingAdvisor":{"enabled":true,"method": "' . str_replace(".toml", "", $strat) . '","candleSize": ' . $candle . ',"historySize":10},"' . str_replace(".toml", "", $strat) . '": ' . json_encode($strategy_load) . ',"backtest":{"daterange":{"from":"' . $from . '","to":"' . $to . '"}},"backtestResultExporter":{"enabled":true,"writeToDisk":false,"data":{"stratUpdates":false,"roundtrips":true,"stratCandles":true,"stratCandleProps":["open"],"trades":false}},"performanceAnalyzer":{"riskFreeReturn":2,"enabled":true},"valid":true}';
